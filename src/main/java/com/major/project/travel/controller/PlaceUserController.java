0c1ef492ca0473b0ea7f959073304af201095ece
Nguyễn Quang Huy <nguyenquanghuy605@gmail.com>
2018-10-16 17:03:57 +0700

    PlaceUserController and UserController

---
+++ src/main/java/com/major/project/travel/controller/PlaceUserController.java
@@ -0,0 +1,13 @@
+package com.major.project.travel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by HUY on 10/16/2018
+ */
+@RestController
+@RequestMapping("/placeUser")
+public class PlaceUserController {
+
+}
---
+++ src/main/java/com/major/project/travel/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.major.project.travel.controller;
+
+/**
+ * Created by HUY on 10/16/2018
+ */
+public class UserController {
+}
